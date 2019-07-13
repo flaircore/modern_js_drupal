ddb5f72cba5c5b531b4653806b653628b7ae54e2
Nicholas Babu <nijoba33@gmail.com>
2019-07-13 21:40:45 +0300

    Update react app for fix for lodash security vulnerability

--- react_js/src/App.js
+++ react_js/src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-console.log('Hello from react_js App.js');
+console.log('Hello from react_js App.js')
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Welcome text="Welcome to Using React text Prop"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -23,6 +24,16 @@ function App() {
       </header>
     </div>
   );
+}
+
+class Welcome extends Component {
+  render() {
+    const { text, textnotthere } = this.props;
+
+    return (
+      <h1 className="App-title">{ text }</h1>
+    )
+  }
 }
 
 export default App;
